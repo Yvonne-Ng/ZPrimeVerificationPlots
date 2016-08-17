3fc662ab8777e3aef71e24adfbaa97a743a3afbd
Yvonne Ng <ywng@uci.edu>
2016-08-17 18:32:44 +0200

    Fixed histogram naming collision, added logscale option, etc
    * Fixed bug in the histogram naming; fixes normalization issues.
    * Added logscale option
    * Unique canvases for each plotting

--- src/yvonne.cxx
+++ src/yvonne.cxx
@@ -12,9 +12,10 @@
 
 using namespace std;
 
-void doPlots(string expression, int nbin, float bin_low, float bin_hi, string outputName, string title="");
+void doPlots(string expression, int nbin, float bin_low, float bin_hi, string outputName, string title="", bool logY=false);
 
-void doPlots(string expression, int nbin, float bin_low, float bin_hi, string outputName, string title){
+void doPlots(string expression, int nbin, float bin_low, float bin_hi, string outputName, string title, bool logY){
+
     vector<string> rootfiles{"mR030_pcut100_evnt10000.root","mR040_pcut100_evnt10000.root","mR050_pcut100_evnt10000.root","mR060_pcut100_evnt10000.root","mR080_pcut100_evnt10000.root","mR100_pcut100_evnt10000.root","mR130_pcut100_evnt10000.root","mR160_pcut100_evnt10000.root","mR190_pcut100_evnt10000.root","mR220_pcut100_evnt10000.root"};//9
     vector<int> mR{30,40,50,60,80,100,130,160,190,220};
     vector<int> colors{1,2,3,4,5,6,7,8,9,11};
@@ -26,12 +27,16 @@ void doPlots(string expression, int nbin, float bin_low, float bin_hi, string ou
     tl->SetFillColor(0);
     tl->SetBorderSize(0);
 
+    TCanvas* canvas = new TCanvas((outputName+"_canvas").c_str(), "", 800, 600);
+    canvas->cd();
+
     bool first = true;
     for (int j=0; j<rootfiles.size(); j++){
         TFile* file = TFile::Open(rootfiles.at(j).c_str());
         TTree* tree = (TTree*)file->Get("truth");
+        int nEntries = tree->GetEntries();
 
-        string histName=expression+"_mR"+to_string(mR.at(j));//e.g: fjet_pt_mR30
+        string histName=outputName+"_mR"+to_string(mR.at(j));//e.g: fjet_pt_mR30
 
         string Drawing=expression+">>"+histName + "(" + to_string(nbin) + "," + to_string(bin_low) + "," + to_string(bin_hi) + ")";
         cout << "Drawing expression: '" << Drawing << "'" << endl;
@@ -44,7 +49,8 @@ void doPlots(string expression, int nbin, float bin_low, float bin_hi, string ou
             continue;
         }
 
-        h->Scale(1./h->Integral());
+        //h->Scale(1./h->Integral());
+        h->Scale(1./nEntries);
 
         h->SetLineColor(colors.at(j));
 
@@ -61,8 +67,9 @@ void doPlots(string expression, int nbin, float bin_low, float bin_hi, string ou
         tl->AddEntry(h,("mR"+to_string(mR.at(j))).c_str(),"LP");
     }
     
-    TCanvas* canvas = (TCanvas*)gROOT->FindObject("c1");
     tl->Draw();
+
+    canvas->SetLogy(logY);
 
     canvas->Update();
     canvas->SaveAs((outputName+"foo.pdf").c_str());
@@ -101,16 +108,21 @@ output_file->Close();
 void plotVariables(void) {
     gStyle->SetOptStat(0);
 
-    int nBinPt=60;
-    float xMinPt=200;
+    int nBinMass=80;
+    float xMinMass=0;
+    float xMaxMass=250;
+
+    int nBinPt=30;
+    float xMinPt=250;
     float xMaxPt=900;
 
-    doPlots("fjet_m[0]/1e3", 80, 0, 250, "fjet_m", ";large-R jet mass [GeV];");
-    doPlots("fjet_pt[0]/1e3", nBinPt, xMinPt, xMaxPt, "fjet_pt");
+    doPlots("fjet_m[0]/1e3", nBinMass, xMinMass, xMaxMass, "fjet_m", ";large-R jet mass [GeV];");
+    doPlots("fjet_pt[0]/1e3", nBinPt, xMinPt, xMaxPt, "fjet_pt", ";large-R jet p_{T} [GeV]", true);
  //   doPlots("jet_pt", 50, 0, 250e3,"test.root");
  //   doPlots("jet_m", 50, 0, 250e3, "test.root");
-    doPlots("fjetTau_m[0]", 50, 0, 250e3, "fjetTau_m");
-    doPlots("fjetTau_pt[0]", nBinPt, 0, xMaxPt, "fjetTau_pt");
+    doPlots("fjetTau_m[0]/1e3", nBinMass, xMinMass, xMaxMass, "fjetTau_m", ";large-R jet mass [GeV]");
+    doPlots("fjetTau_pt[0]/1e3", 50, 0, 1.2e3, "fjetTau_pt","",false);
+    doPlots("fjetTau_pt[0]/1e3", 50, 0, 1.2e3, "fjetTau_pt_log","",true);
     doPlots("jetNOleading_pt[0]", nBinPt, 0, xMaxPt,"jetNOleading_pt");
     doPlots("jetNOleading_m[0]", 50, 0, 250e3, "jetNOleading_m");
     doPlots("jetNOleading_pt[0]",nBinPt,0,xMaxPt, "jetNOleading_pt");
